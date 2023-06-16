389190d7c3985d6492c05f9146824c82b0827ca3
Ahmad Alturki <aimt1@protonmail.com>
2023-06-16 15:09:50 +0300

    feat: add basic telegram bot

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,77 @@ import * as dotenv from "dotenv";
 import { getOrders } from "./agent.ts";
 import { queryDoc, storeDoc } from "./manage-docs.ts";
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { Telegraf } from "telegraf";
+import { Model as ChatWithTools } from "./models/chatWithTools.ts";
 
 dotenv.config();
 
-getOrders();
+// TODO: study removal, this will probably be replaced by the telegram bot 
+// await getOrders();
 
-await storeDoc(new PDFLoader("docs/april-2023.pdf"), "test-namespace");
 
-await queryDoc([
-  "What does the pdf talk about?",
-  "what is the main cause for inflation?",
-], "test-namespace");
+// TODO: expose endpoint to store pdf in pinecone
+// a possible solution to the problem of deleting pdfs from pinecone
+// is to store the pdfs with their name as the namespace
+// await storeDoc(new PDFLoader("docs/april-2023.pdf"), "test-namespace");
+
+
+// TODO: study removal, this will probably be replaced by the telegram bot 
+// await queryDoc([
+//   "What does the pdf talk about?",
+//   "what is the main cause for inflation?",
+// ], "test-namespace");
+
+
+// TODO: run telegram bot
+const telegramToken = process.env.TELEGRAM_TOKEN!;
+
+const bot = new Telegraf(telegramToken);
+let model = new ChatWithTools();
+
+bot.start((ctx) => {
+  ctx.reply("Welcome to my Telegram bot!");
+});
+
+bot.help((ctx) => {
+  ctx.reply("Send me a message and I will echo it back to you.");
+});
+
+bot.on("message", async (ctx) => {
+  const text = (ctx.message as any).text;
+
+  if (!text) {
+    ctx.reply("Please send a text message.");
+    return;
+  }
+
+  console.log("Input: ", text);
+
+  await ctx.sendChatAction("typing");
+  try {
+    const response = await model.call(text);
+
+    await ctx.reply(response);
+  } catch (error) {
+    console.log(error);
+
+    const message = JSON.stringify(
+      (error as any)?.response?.data?.error ?? "Unable to extract error"
+    );
+
+    console.log({ message });
+
+    await ctx.reply(
+      "Whoops! There was an error while talking to OpenAI. Error: " + message
+    );
+  }
+});
+
+
+bot.launch().then(() => {
+  console.log("Bot launched");
+});
+
+process.on("SIGTERM", () => {
+  bot.stop();
+});
---
+++ src/models/chatWithTools.ts
@@ -0,0 +1,58 @@
+import { AgentExecutor, Tool, initializeAgentExecutor } from "langchain/agents";
+// import { Tool } from "langchain/tools";
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { BufferMemory } from "langchain/memory";
+import { Configuration } from "openai";
+import { OpenAIApi } from "openai";
+import { googleTool } from "./tools/google.ts";
+
+const openAIApiKey = process.env.OPENAI_API_KEY!;
+
+const params = {
+  verbose: true,
+  temperature: 1,
+  openAIApiKey,
+  modelName: "gpt-3.5-turbo-16k",
+  maxConcurrency: 1,
+  maxTokens: 1000,
+  maxRetries: 5,
+};
+
+export class Model {
+  public tools: Tool[];
+  public executor?: AgentExecutor;
+  public openai: OpenAIApi;
+  public model: ChatOpenAI;
+
+  constructor() {
+    const configuration = new Configuration({
+      apiKey: openAIApiKey,
+    });
+
+    this.tools = [googleTool];
+    this.openai = new OpenAIApi(configuration);
+    this.model = new ChatOpenAI(params, configuration);
+  }
+
+  public async call(input: string) {
+    if (!this.executor) {
+      this.executor = await initializeAgentExecutor(
+        this.tools,
+        this.model,
+        "chat-conversational-react-description",
+        true
+      );
+      this.executor.memory = new BufferMemory({
+        returnMessages: true,
+        memoryKey: "chat_history",
+        inputKey: "input",
+      });
+    }
+
+    const response = await this.executor!.call({ input });
+
+    console.log("Model response: " + response);
+
+    return response.output;
+  }
+}
