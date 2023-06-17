d5a2fa3e90d4549f42e89da80dccba6c72fe81c2
Ahmad Alturki <aimt1@protonmail.com>
2023-06-17 09:36:24 +0300

    refactor: move tools into separate files

--- src/models/chatWithTools.ts
+++ src/models/chatWithTools.ts
@@ -1,11 +1,12 @@
 import { AgentExecutor, initializeAgentExecutorWithOptions } from "langchain/agents";
-import { DynamicTool, Tool } from "langchain/tools";
+import { Tool } from "langchain/tools";
 import { ChatOpenAI } from "langchain/chat_models/openai";
 import { BufferMemory } from "langchain/memory";
 import { Configuration } from "openai";
 import { OpenAIApi } from "openai";
 import { vectorStoreTool } from "./tools/vectorestoreQA.ts";
-import { Api } from "@/types/Api.ts";
+import { reportIssuesTool } from "./tools/reportIssues.ts";
+import { searchServicesTool } from "./tools/searchServices.ts";
 
 const openAIApiKey = process.env.OPENAI_API_KEY!;
 
@@ -32,71 +33,8 @@ export class Model {
 
     this.tools = [
       vectorStoreTool,
-      new DynamicTool({
-        name: 'search hotel services',
-        description: 'a function to look up the currently available services in the hotel.',
-        func: async () => {
-          try {
-            const api = new Api({
-              baseUrl: `https://determined-boot-55a0a0a0d0.strapiapp.com/api`,
-              baseApiParams: {
-                headers: {
-                  'Authorization': `Bearer ${process.env.STRAPI_API_KEY}`,
-                  'accept': 'application/json',
-                }
-              }
-            });
-            const resp = await api.services.getServices();
-
-            return JSON.stringify(resp.data);
-          } catch (e) {
-            console.error('strapi caught error', e);
-            return "No good search result found";
-          }
-        }
-      }),
-      new DynamicTool({
-        name: 'report an issue',
-        description: `A function to report an issue to the hotel staff. The guest's room number should be known before calling this function. The input is a JSON string matching the following schema \`\`\`typescript
-          roomn_number: number;
-          title?: string;
-          status?: "Active" | "Fixing" | "Solved";
-          resolutiontime?: number;
-          department?: "Housekeeping" | "Engineering" | "IT" | "Fooddepartment";
-          description?: string;
-        \`\`\`.`,
-        func: async (input) => {
-          try {
-            const api = new Api({
-              baseUrl: `https://determined-boot-55a0a0a0d0.strapiapp.com/api`,
-              baseApiParams: {
-                headers: {
-                  'Authorization': `Bearer ${process.env.STRAPI_API_KEY}`,
-                  'accept': 'application/json',
-                }
-              }
-            });
-            console.log('input 32', input);
-            const parsedInput = JSON.parse(input);
-
-            if (!parsedInput.room_number) {
-              return "Please provide a room number in the JSON object input. Ask the user for the room number and then call this function again.";
-            }
-
-            const resp = await api.issuesReports.postIssuesReports({
-              data: {
-                ...parsedInput,
-                roomnumber: parsedInput.room_number,
-              }
-            });
-
-            return "Issue reported successfully";
-          } catch (e) {
-            console.error('strapi caught error while reporting an issue', e);
-            return "An error occured while reporting an issue";
-          }
-        }
-      }),
+      searchServicesTool,
+      reportIssuesTool,
     ];
     this.openai = new OpenAIApi(configuration);
     this.model = new ChatOpenAI(params, configuration);
